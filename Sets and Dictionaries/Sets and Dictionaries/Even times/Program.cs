d269b62766568beb9d43b7fad6fdd9e7571acd83
Persiyan93 <persiyan.georgiev@gmail.com>
2020-06-04 18:24:18 +0300

    new

---
+++ Sets and Dictionaries/Sets and Dictionaries/Even times/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Even_times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfInputs = int.Parse(Console.ReadLine());
+            Dictionary<int, int> numbers = new Dictionary<int, int>();
+            for (int i = 0; i < countOfInputs; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                if (numbers.ContainsKey(currentNumber))
+                {
+                    numbers[currentNumber]++;
+                }
+                else
+                {
+                    numbers[currentNumber] = 1;
+                }
+            }
+            foreach (var item in numbers)
+            {
+                if (item.Value % 2 == 0)
+                {
+                    Console.WriteLine(item.Key);
+                    return;
+                }
+            }
+
+        }
+    }
+}
---
+++ Sets and Dictionaries/Sets and Dictionaries/Periodic Table/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfInputs = int.Parse(Console.ReadLine());
+            SortedSet<string> elemnts = new SortedSet<string>();
+            for (int i = 0; i < countOfInputs; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                foreach (var item in input)
+                {
+                    elemnts.Add(item);
+                }
+
+            }
+            Console.WriteLine(string.Join(" ", elemnts));
+        }
+        
+    }
+}
