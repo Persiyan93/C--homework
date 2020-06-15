ff41092ed2cbf8a0946bfe18bf4cb7b7f4a4d4f7
Persiyan93 <persiyan.georgiev@gmail.com>
2020-06-15 18:47:08 +0300

    TriFunction

---
+++ Functional Programing/TriFuncition/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace TriFuncition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine().Split(" ").ToList();
+            Func<string, int> Charsum = x =>
+            {
+                int sum = 0;
+                for (int i = 0; i < x.Length; i++)
+                {
+                    sum =sum+ (int)x[i];
+                    
+                }
+                return sum;
+
+            };
+            
+            for (int i = 0; i < names.Count; i++)
+            {
+                if (Charsum(names[i])==number)
+                {
+                    Console.WriteLine(names[i]);
+                    break;
+                }
+            }
+        }
+    }
+}
